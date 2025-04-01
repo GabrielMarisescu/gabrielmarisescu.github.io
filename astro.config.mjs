80743c45a0ca78acb41236d3f5d265a5abaf1440
Gabriel Marisescu <ionutgabriel.marisescu@eng.it>
2025-04-01 20:45:26 +0200

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   output: "static",
   trailingSlash: "always",
-  site: "http://gabrielmarisescu.github.io/",
-  base: "my-repo",
+  site: "https://gabrielmarisescu.github.io",
 
   // Single page, no prefetch needed
   prefetch: false,
